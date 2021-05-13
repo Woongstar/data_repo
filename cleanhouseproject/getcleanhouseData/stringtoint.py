0e8874a818a079d27b77cef8e309e358957fd70c
Jiwoong Yang <qualtypo@gmail.com>
2021-05-13 11:48:14 +0900

    string to numeric latitude

---
+++ cleanhouseproject/getcleanhouseData/stringtoint.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = pd.read_csv("./jejucleanhouseutf.csv")
+
+
+# def save(df, filename):
+#     writer = pd.ExcelWriter(filename)
+#     df.to_excel(writer, "sheet1")
+#     writer.save()
+#
+# save(data, "jejucleanhouse.xlsx")
+
+# data["latitude"][806] == "33.3032512"
+data["latitude"] = pd.to_numeric(data["latitude"])
+# print(data["latitude"][807])
+# print(data["latitude"][806])
+# print(data)
+print(data.dtypes)
+data.to_json("./jejucleanhousedata.json",orient="columns")
