6e326bc72515914ff8e377ee81e6b59452ffe9f2
Jiwoong Yang <qualtypo@gmail.com>
2021-05-17 10:12:43 +0900

    get seoguipo recycle data

--- cleanhouseproject/getcleanhouseData/getrecyclecenter.py
+++ cleanhouseproject/getcleanhouseData/getrecyclecenter.py
@@ -10,6 +10,7 @@ soup = BeautifulSoup(res.text,"html.parser")
 
 tables = soup.select('table')
 table = tables[0]
+table1 = tables[1]
 
 # data_rows = soup.find("table",attrs={"class":"table dataTable"}).find("tbody").find_all("tr")
 # for row in data_rows:
@@ -19,18 +20,29 @@ table = tables[0]
 #     data = [column.get_text for column in columns]
 #     print(data)
 # print(data_rows)
+
+#제주시 재활용센터 데이터
 table_html = str(table)
 recycle_list = pd.read_html(table_html)
 recycledata = recycle_list[0]
 # df = pd.DataFrame(recycle_list)
 
+#서귀포시 재활용센터 데이터
+table1_html = str(table1)
+recycle_list1 = pd.read_html(table1_html)
+recycledata1 = recycle_list1[0]
+recycledata1.drop(columns=["읍면동","특수시책","위치","운영시간","운영개시일"], inplace=True)
+recycledata1["location"] = recycledata["명칭"]
+recycledata1["address"] = recycledata["도로명주소"]
+recycledata1.drop(columns=["명칭","도로명주소"], inplace=True)
+recycledata1.to_json("./recycledata1.json",orient='records')
 
-recycledata.drop(columns=["읍면동","특수시책","위치","운영시간","운영개시일"], inplace=True)
-recycledata["location"] = recycledata["명칭"]
-recycledata["address"] = recycledata["도로명주소"]
-recycledata.drop(columns=["명칭","도로명주소"], inplace=True)
-print(recycledata)
-recycledata.to_json("./recycledata.json",orient='records')
+# recycledata.drop(columns=["읍면동","특수시책","위치","운영시간","운영개시일"], inplace=True)
+# recycledata["location"] = recycledata["명칭"]
+# recycledata["address"] = recycledata["도로명주소"]
+# recycledata.drop(columns=["명칭","도로명주소"], inplace=True)
+# print(recycledata)
+# recycledata.to_json("./recycledata.json",orient='records')
 # print(type(recycledata))
 # col_name = ['colname1', 'colname2',"colname3","colname4"]
 # df = pd.DataFrame(recycle_list[0], columns=col_name)
