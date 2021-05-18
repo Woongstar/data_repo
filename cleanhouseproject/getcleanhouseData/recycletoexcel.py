50d2d1c5dc7159787779d8bfbeb709e44758c647
Jiwoong Yang <qualtypo@gmail.com>
2021-05-18 23:17:24 +0900

    add recyclejson

--- cleanhouseproject/getcleanhouseData/recycletoexcel.py
+++ cleanhouseproject/getcleanhouseData/recycletoexcel.py
@@ -1,3 +1,11 @@
 import pandas as pd
-df = pd.read_json("./jejurecycledata.json")
-df.to_excel("./jejurecycleexcel.xlsx")
+# df = pd.read_json("./jejurecycledata.json")
+# df.to_excel("./jejurecycleexcel.xlsx")
+df = pd.read_csv("./jejurecycleexcelcsv.csv")
+
+df.drop(columns=["Unnamed: 0"], inplace=True)
+# print(df)
+# df["type"]
+df["type"]  = "recycle"
+print(df.columns)
+df.to_json("./jejurecyclejson.json", orient="records")
