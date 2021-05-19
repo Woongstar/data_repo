a399a73a4cb6b34b7829056e97503db1acb0a16f
Jiwoong Yang <qualtypo@gmail.com>
2021-05-20 00:07:19 +0900

    edit data calumn name

--- cleanhouseproject/getcleanhouseData/editdata.py
+++ cleanhouseproject/getcleanhouseData/editdata.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("./ejejejej.csv")
-df.drop(columns=["Unnamed: 0"],inplace=True)
+# df = pd.read_csv("./ejejejej.csv")
+df = pd.read_json("./cleandata.json")
+# df.drop(columns=["Unnamed: 0"],inplace=True)
+df.rename(columns={"longtitude":"longitude"},inplace=True)
 df.to_json("./cleandata.json",orient="records")
 print(df)
